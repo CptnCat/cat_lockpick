bae5b74534652b40a62ea5e208ade1c6a406d14d
EpicCat <78860645+CptnCat@users.noreply.github.com>
2025-02-11 14:23:40 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = {}
 Config.VersionCheck = true
 Config.Framework = 'esx' -- Supports ESX & QB
 
+Config.UseT3Minigame = true -- If you don't want to use the minigame from t3development, just set it to false
+
 Config.LockPickItem = 'lockpick' -- Specify the item name for the lockpick
 Config.RemoveLockpickOnUse = true -- Should the lockpick be removed after use?
 Config.RemoveOnlyOnFailure = false -- Should the lockpick be removed only when the minigame fails? (Config.RemoveLockpickOnUse must be enabled)
